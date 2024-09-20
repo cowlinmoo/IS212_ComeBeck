b52447a53fc6af1d96f8143281621186bf3afccb
molincok <colinmok1000@gmail.com>
2024-09-20 10:56:50 +0800

    CB-57-Fix wrong authentication endpoint urL

--- backend/routers/EmployeeRouter.py
+++ backend/routers/EmployeeRouter.py
@@ -11,8 +11,7 @@ EmployeeRouter = APIRouter(
 )
 
 @EmployeeRouter.get("/", response_model=List[EmployeeSchema])
-def get_all_employees(employee_service: EmployeeService = Depends(),
-                      current_user: dict = Depends(role_required(1, 2))):
+def get_all_employees(employee_service: EmployeeService = Depends()):
     return employee_service.get_all_employees()
 
 @EmployeeRouter.get("/{staff_id}", response_model=EmployeeSchema)
@@ -24,9 +23,11 @@ def create_employee(employee: EmployeeSchema, employee_service: EmployeeService 
     return employee_service.create_employee(employee)
 
 @EmployeeRouter.put("/{staff_id}", response_model=EmployeeSchema)
-def update_employee(staff_id: int, employee: EmployeeSchema, employee_service: EmployeeService = Depends()):
+def update_employee(staff_id: int, employee: EmployeeSchema, employee_service: EmployeeService = Depends(),
+                    current_user: dict = Depends(role_required(1, 2))):
     return employee_service.update_employee(staff_id, employee)
 
 @EmployeeRouter.delete("/{staff_id}", response_model=EmployeeSchema)
-def delete_employee(staff_id: int, employee_service: EmployeeService = Depends()):
+def delete_employee(staff_id: int, employee_service: EmployeeService = Depends(),
+                    current_user: dict = Depends(role_required(1, 2))):
     return employee_service.delete_employee_by_staff_id(staff_id)
