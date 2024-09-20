ddf0f195f758be014fac9b12f18cb0ab337637b6
dsaid <joen_tan_from.tp@tech.gov.sg>
2024-09-20 10:21:10 +0800

    CB-56 created arrangement table

---
+++ backend/models/ArrangementModel.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, BigInteger, Text, Boolean, ForeignKey
+from models import EntityMeta
+
+
+class Arrangement(EntityMeta):
+    __tablename__ = 'arrangement'
+
+    arrangement_id = Column(BigInteger, primary_key=True)
+    reason = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    recurring = Column(Boolean, nullable=False)
+    application_id = Column(BigInteger, ForeignKey(
+        'application.application_id'), nullable=False)
+
+    class Config:
+        orm_mode = True
