3cd66f8bb39ccd50bda112a69681a1e861b42eed
Daryl Yoon Kaile <84636602+darylyoon@users.noreply.github.com>
2024-10-05 17:23:32 +0800

    CB-62 View my applications

--- frontend/src/app/approvals/page.tsx
+++ frontend/src/app/approvals/page.tsx
@@ -4,17 +4,55 @@ import useAuth from "@/lib/auth";
 import { Header } from "@/components/core/header/Header";
 import Approvals from "@/components/arrangement/approvals/Approvals";
 import SideBar from "@/components/core/sidebar/SideBar";
+import { GetApplicationOfStaff } from "./api";
+import { useEffect, useState } from "react";
+
+export interface Application {
+  application_id: number;
+  reason: string;
+  description: string;
+  created_on: string;
+  last_updated_on: string;
+  status: string;
+  staff_id: number;
+  approver_id: number;
+  recurring: boolean;
+  events: Event[];
+}
+
+export interface Event {
+  event_id: number;
+  requested_date: string;
+  location: string;
+}
 
 export default function Component() {
 
   const { token, userId, pageLoading } = useAuth();
 
+  const [applications, setApplications] = useState<Application[]>([]);
+
+  useEffect(() => {
+    if (userId) {
+      try {
+        console.log("Fetching staff application...");
+        const fetchData = async () => {
+          const data = await GetApplicationOfStaff(101);
+          setApplications(data);
+        };
+        fetchData();
+      } catch (error) {
+        console.error("Failed to fetch staff application:", error);
+      }
+    }
+  }, [userId]);
+
   if (pageLoading || (!pageLoading && token === undefined)) {
     return <div className='flex items-center justify-center h-screen w-screen'>Loading...</div>;
   }
 
-  console.log('Token:', token);
-  console.log('User ID:', userId);
+  console.log(applications);
+
 
   return (
     <div className="flex h-screen text-black bg-gray-100">
@@ -26,7 +64,7 @@ export default function Component() {
         {/* Header */}
         <Header/>
         {/* Page Content */}
-        <Approvals />
+        <Approvals data={applications} />
       </div>
     </div>
   )
