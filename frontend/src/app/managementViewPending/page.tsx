f59490c7ddc241a290b690531cf8b0dec5a02cb9
molincok <colinmok1000@gmail.com>
2024-10-26 22:21:44 +0800

    update branch

--- frontend/src/app/managementViewPending/page.tsx
+++ frontend/src/app/managementViewPending/page.tsx
@@ -14,16 +14,22 @@ export interface Application {
     created_on: string;
     last_updated_on: string;
     status: string;
-    staff_id: number;
+    application_state: string;
     approver_id: number;
     recurring: boolean;
     events: Event[];
+    staff: {
+      staff_id: number;
+      staff_fname: string;
+      staff_lname: string;
+    }
   }
-  
+
   export interface Event {
     event_id: number;
     requested_date: string;
     location: string;
+    application_hour: string;
   }
 
 export default function Component() {
