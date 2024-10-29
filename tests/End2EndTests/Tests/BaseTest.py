7b50add9f0ee6eacb6e5f28671ab620b2368d2fb
molincok <colinmok1000@gmail.com>
2024-10-30 00:18:06 +0800

    set to headless

--- tests/End2EndTests/Tests/BaseTest.py
+++ tests/End2EndTests/Tests/BaseTest.py
@@ -5,7 +5,7 @@ class BaseTest:
     @pytest.fixture(scope="function", autouse=True)
     def setup_page(self, playwright: Playwright):
         """Fixture to set up browser and page context for each test."""
-        self.browser: Browser = playwright.chromium.launch(headless=False)
+        self.browser: Browser = playwright.chromium.launch(headless=True)
         self.context = self.browser.new_context()  # Separate context for parallel compatibility
         self.page: Page = self.context.new_page()
         yield self.page  # Provide the page to each test
